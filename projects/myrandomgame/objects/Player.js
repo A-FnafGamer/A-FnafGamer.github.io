859bf2d5c1ac252154af3b820eea937ac1dd933a
A-FnafGamer <infofnafdev@gmail.com>
2018-02-06 08:23:36 +0000

    Update Player.js

--- projects/myrandomgame/objects/Player.js
+++ projects/myrandomgame/objects/Player.js
@@ -1,8 +1,8 @@
-{
+var Player = {
 "x":0,
 "y":0,
 "width":10,
 "height":10,
 "Draw":"gg",
-"Erase":"coming soon"
+"Erase":function(EraseType){if (EraseType == "Content"){this.x = 0; this.y = 0;}else if(EraseType == "Graphics"){console.log("notavailable")}}
 }

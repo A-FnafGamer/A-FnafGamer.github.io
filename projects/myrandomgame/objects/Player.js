1de9372a13418133a1678cd5c681322ff9b0b02d
A-FnafGamer <infofnafdev@gmail.com>
2018-02-20 21:48:37 +0000

    Delete Player.js

--- projects/myrandomgame/objects/Player.js
+++
@@ -1,7 +0,0 @@
-this.Player = new function(){
-this.x = 0;
-this.y = 0;
-this.width = 10;
-this.height = 10;
-"Erase":function(EraseType){if (EraseType == "Content"){this.x = 0; this.y = 0;}else if(EraseType == "Graphics"){console.log("notavailable")}}
-}

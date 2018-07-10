263d3685a546318a05693ce9376cf764384d5ec9
A-FnafGamer <infofnafdev@gmail.com>
2018-07-10 20:10:37 +1000

    Update and rename data.json to data.js

---
+++ data/data.js
@@ -0,0 +1,21 @@
+var _DATA_ =
+{
+"Paths":
+  {
+  "Resources":"/data/res",
+  "Scripts":"/data/scripts/",
+  "Projects":"/projects"
+  },
+"Keys":
+  [
+    0000
+  ],
+"Projects":
+  [
+    {
+      Name:"Canvas Game",
+      Description:"This is a Game that i am working on. it is just a test and it is not intendede to have textures or fancy graphics.\nthis is made just to understand more how events work =D"
+      
+    }
+  ]
+};

156063c4fedcdec42d365031c6fcb63f6f99be29
A-FnafGamer <infofnafdev@gmail.com>
2018-07-10 23:58:27 +1000

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,21 +1,23 @@
 var _DATA_ =
 {
-"Paths":
+Paths:
   {
-  "Resources":"/data/res",
-  "Scripts":"/data/scripts/",
-  "Projects":"/projects"
+  Resources : "/data/res/",
+  Scripts : "/data/scripts/",
+  Projects : "/data/projects/"
   },
-"Keys":
+Keys:
   [
-    0000
+    ""
   ],
-"Projects":
+Projects:
   [
     {
-      Name:"Canvas Game",
-      Description:"This is a Game that i am working on. it is just a test and it is not intendede to have textures or fancy graphics.\nthis is made just to understand more how events work =D"
-      
+      Name : "Canvas Game",
+      Description : "This is a Game that i am working on. it is just a test and it is not intendede to have textures or fancy graphics.\nthis is made just to understand more how events work =D",
+      Thumbnail : null,
+      Files : "CanvasGame_v1.0",
+      SourceCodeAccessKey : "abdc85GHC75IJBHy7t76GGttrf65fgFffF67tfy6r7yFghgf67HGc"
     }
   ]
 };

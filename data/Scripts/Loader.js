51ff7682ad5589f708682049ab8707f62420a9fe
A-FnafGamer <infofnafdev@gmail.com>
2018-11-26 17:57:50 +1100

    Delete Loader.js

--- data/Scripts/Loader.js
+++
@@ -1,8 +0,0 @@
-function Load(Request, Method, dataType, callback)
-{
- XReq = new XMLHttpRequest();
- XReq.overrideMimeType(dataType);
- XReq.open(Method, Request);
- XReq.onreadystatechange = callback;
- XReq.send();
-}

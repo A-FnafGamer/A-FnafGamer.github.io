4f965c88ac2efe1792791fca5803a4737b96ec43
A-FnafGamer <infofnafdev@gmail.com>
2018-11-26 22:21:09 +1100

    Create index.js

---
+++ theDerpyGame/Release/1.0/index.js
@@ -0,0 +1,28 @@
+this.onload = Startup();
+
+var Config = {
+FPS:100,
+CDM:"per_Frame",
+CanvasScale:"fill_Screen",
+CustomCDRR:60,
+CustomResCanvasWidth:1080,
+CustomResCanvasHieght:720,
+keysDown
+};
+
+function Startup() {
+  if (Config.CDM === "per_Two_Frames) {
+    Config.CDRR = Config.FPS/2
+  }else if (Config.CDM === "per_Frame") {
+    Config.CDRR = Config.FPS;
+  }else if (Config.CDM === "Per_Half_Frame") {
+    Config.CDRR = 2*Config.FPS;
+  }else if (Config.CDM === "Custom") {
+    Config.CDRR = Config.CustomCDRR;
+  }
+  
+  
+  
+  this.tick = setInterval(tick(), 1000/Config.CDRR);
+  this.
+}

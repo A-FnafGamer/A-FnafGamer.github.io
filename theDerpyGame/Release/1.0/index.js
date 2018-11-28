3ced129e9ec84766f218209710b75c6f29d16970
A-FnafGamer <infofnafdev@gmail.com>
2018-11-29 09:48:48 +1100

    Update index.js

--- theDerpyGame/Release/1.0/index.js
+++ theDerpyGame/Release/1.0/index.js
@@ -10,7 +10,7 @@ keysDown:[]
 };
 
 //the game's vital Components:
-var Main_Scene = document.getElementById("Scene");
+var Main_Scene = document.getElementsByTagName("Scene")[0];
 console.log(Main_Scene);
 
 //the game's components(Objects)
